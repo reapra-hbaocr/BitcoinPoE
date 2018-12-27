9de2eaab1f12c84d7a2b5cc5e4c37d1a581d1bce
bao.duong@tech.reapra.sg <duonghuynhbao@Duongs-MacBook-Pro.local>
2018-12-27 12:20:48 +0700

    add_config file

--- config.js
+++ config.js
@@ -1,7 +1,21 @@
-module.exports={
-    prk_wif:'cUcTYjjc5hcivJdqjhvg57wSnpsfgaxzTWFtP8cP3J8cYqNPSAQF'
 
+const bitcoin = require('bitcoinjs-lib');
+module.exports.prvk_wif='cUcTYjjc5hcivJdqjhvg57wSnpsfgaxzTWFtP8cP3J8cYqNPSAQF';
+module.exports.get_wallet_info=get_wallet_info;
+
+function get_wallet_info(prk_wif,network){
+    let wallet1 = bitcoin.ECPair.fromWIF(prk_wif, network);
+    let p2pkh1 = bitcoin.payments.p2pkh({ pubkey: wallet1.publicKey, network: network });
+    let p2pkh1_addr = p2pkh1.address;
+    let ret={};
+    ret.wallet =wallet1;
+    ret.p2pkh =p2pkh1;
+    ret.p2pkh_addr=p2pkh1_addr;
+    return ret;
 }
+
+
+
 // const p2pkh1 =bitcoin.payments.p2pkh({ pubkey: wallet1.publicKey,network:bitcoin.networks.testnet});
 // let p2pkh1_addr=p2pkh1.address;
 // let  wallet2 = bitcoin.ECPair.makeRandom({network:TestNet,rng:()=>{
--- index.js
+++ index.js
@@ -5,26 +5,23 @@ const TestNet = bitcoin.networks.testnet;
 var embed_tx = require('./tx_embeded_data');
 var bitcoin_composer = require('./bitcoin_tx_composer');
 var bitcoinSubmit= require('./submit-signed-tx');
-
+var mywallet = require('./config');
 var bSubmit = new bitcoinSubmit(false);
 
-var fname = 'download.html';
+var fname = 'info.txt';
 let chunks = embed_tx.file_to_chunks(fname, 65);
 
-const wallet1 = bitcoin.ECPair.fromWIF('cUcTYjjc5hcivJdqjhvg57wSnpsfgaxzTWFtP8cP3J8cYqNPSAQF', TestNet);
-const p2pkh1 = bitcoin.payments.p2pkh({ pubkey: wallet1.publicKey, network: TestNet });
-let p2pkh1_addr = p2pkh1.address;
-console.log('p2pkh1 ', p2pkh1_addr);
+var my_prvK_wif =mywallet.prvk_wif;
+var my_acc = mywallet.get_wallet_info(my_prvK_wif,TestNet);
+let wallet1 = my_acc.wallet;
 
-//https://api.blockcypher.com/v1/btc/test3/txs/39bd264bcacc9afd4bcde3492d25a48f69b3ed3ad418920338e3301c74920755?limit=50&includeHex=true
-//addr:'mgtRUQJgZEJq2bWjm5Cyncfk8LQKqexmjq',
 let utxo_info = {
-    value: 11608044,
+    value: 11572044,
     txid: 'f7500116393a275d919b8266b49576835d37bdce4560a9fc5319b2dc178d17ea',
     vout_idx: 8
 }
-//fb4ae9c3b69ab621ab6b2221b55b6d3f0589f7906f1b02d5232236efdd15449a
-let ret=bitcoin_composer.create_p2ms_tx_info(wallet1,utxo_info,chunks,10,7000,1000);
+
+let ret=bitcoin_composer.create_p2ms_tx_info(wallet1,utxo_info,chunks,10,6600,0);
 console.log('fee = ',ret.fee);
 console.log('TxID:  ',ret.txId);
 fs.writeFileSync('o.txt',ret.hexTx);
